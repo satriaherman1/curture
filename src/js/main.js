12c04c40b49ca9506859ad9089971bdc8b05142e
satriaherman1 <muhammadsatriaherman@gmail.com>
2023-02-16 21:55:02 +0700

    add navbar

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+const wrapper = document.querySelector(".sponsorship-wrapper");
+const imageList = document.querySelector(".sponsorship");
+const imageWidth = imageList.offsetWidth;
+const wrapperWidth = wrapper.offsetWidth;
+
+const checkSponsorshipPosition = () => {
+  if (imageWidth + imageList.offsetLeft < 0) imageList.style.left = `${wrapperWidth} px`;
+
+  requestAnimationFrame(checkSponsorshipPosition);
+};
+
+// checkSponsorshipPosition();
+const addNavbarBg = () => {
+  const navbar = document.querySelector("nav");
+  const smallNavbar = document.querySelector(".small-nav");
+
+  if (!smallNavbar.classList.contains("active")) {
+    if (window.scrollY > 10) {
+      navbar.style.backgroundColor = "white";
+      navbar.classList.add("shadow");
+    } else {
+      navbar.style.backgroundColor = "transparent";
+      navbar.classList.remove("shadow");
+    }
+  } else {
+    navbar.style.backgroundColor = "white";
+  }
+};
+
+const handleClickNav = (nav) => {
+  const navbar = document.querySelector("nav");
+  const smallNavbar = document.querySelector(".small-nav");
+
+  if (!smallNavbar.classList.contains("active")) {
+    smallNavbar.style.top = navbar.clientHeight + "px";
+    navbar.classList.remove("shadow");
+  } else {
+    smallNavbar.style.top = -100 + "vh";
+    navbar.classList.add("shadow");
+  }
+  nav.classList.toggle("active");
+  smallNavbar.classList.toggle("active");
+};
+
+window.addEventListener("scroll", addNavbarBg);
